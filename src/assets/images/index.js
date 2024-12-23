7168ad814eb0f278c932286458e743a7d694cb55
Sandipan Rakshit <sandipanrakshit6@gmail.com>
2024-12-23 19:50:22 +0530

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+export {default as Logo} from './Branding-01.svg'
+export {default as image1} from './IMAGE (4).png'
+export {default as image2} from './IMAGE (5).png'
+export {default as image3} from './IMAGE (6).png'
+export {default as image4} from './IMAGE (7).png'
+export {default as image5} from './IMAGE (8).png'
+export {default as clients} from './clients.png';
+export {default as footer} from './footer.jpg'

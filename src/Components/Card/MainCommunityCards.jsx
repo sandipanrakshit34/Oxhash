68824648c06b1e8e249764ed0cfc475638e79e7c
Sandipan Rakshit <sandipanrakshit6@gmail.com>
2024-12-23 19:50:59 +0530

    Add files via upload

---
+++ src/Components/Card/MainCommunityCards.jsx
@@ -0,0 +1,65 @@
+import CommunityCards from "./CommunityCards";
+import Card1 from "../../assets/images/Players (1).jpeg";
+import Card2 from "../../assets/images/Players (2).jpeg";
+import Card3 from "../../assets/images/Players (3).jpeg";
+
+import GroupImage from "../../assets/images/Group 3.png";
+
+function MainCommunityCards() {
+  return (
+    <div className="w-full max-w-[1920px] flex flex-col justify-center items-center mt-16">
+        <h1 className="w-[90%] text-center md:text-[96px] text-[50px] pt-16 leading-none font-bold text-white py-16">
+        Our community <span className="text-[#1AA9F9]">is playing</span>
+        </h1>
+      <div className="md:w-[90%] w-[90%] grid grid-cols-1 md:grid-cols-3 gap-10 my-14">
+        <CommunityCards
+          image={Card3}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+        <CommunityCards
+          image={Card1}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+        <CommunityCards
+          image={Card2}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+      </div>
+
+      <div className="md:w-[90%] w-[90%] grid grid-cols-1 md:grid-cols-3 gap-10">
+        <CommunityCards
+          image={Card2}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+        <CommunityCards
+          image={Card1}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+        <CommunityCards
+          image={Card3}
+          playerImages={GroupImage}
+          gameName="Skyborne"
+          gamelink="ToomuchLag"
+          gamePrice="242eth"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MainCommunityCards;
---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,31 @@
+import Button from "../Button/Button";
+import Header from "../Header/Header";
+
+function HeroSection() {
+  return (
+    <div
+      className="w-full max-w-[1920px] flex flex-col justify-center items-center"
+      id="HeroSection"
+    >
+      <Header />
+
+      <div className="w-[80%] text-center flex flex-col justify-center items-center">
+        <h1 className="md:text-[118px] text-[65px] pt-16 leading-none font-semibold text-white">
+          Build On The Edge <br /> Of{" "}
+          <span className="text-[#1AA9F9]">Gaming</span>
+        </h1>
+        <p className="md:w-[60%] w-[100%] md:text-[20px] font-light text-[18px] py-8 pb-16 leading-tight text-[#FFFFFF]">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of{" "}
+        </p>
+        <div className="flex gap-5 pb-16">
+          <Button value="Start Building" />
+          <Button value="Explore Docs" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
